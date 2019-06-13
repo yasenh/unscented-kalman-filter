bd713a97d481b975e7ece02f357fca46b27808fa
yasenh <yasenhu789@gmail.com>
2019-06-12 22:43:54 -0400

    start to implement UKF

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,6 @@
 // Create simple 3d highway environment using PCL
 // for exploring self-driving car sensors
 
-//#include "render/render.h"
 #include "highway.h"
 
 int main(int argc, char** argv) {
@@ -13,26 +12,24 @@ int main(int argc, char** argv) {
 	// set camera position and angle
 	viewer->initCameraParameters();
 	float x_pos = 0;
-	viewer->setCameraPosition (x_pos-26, 0, 15.0, x_pos+25, 0, 0, 0, 0, 1);
+	viewer->setCameraPosition(x_pos-26, 0, 15.0, x_pos+25, 0, 0, 0, 0, 1);
 
 	Highway highway(viewer);
 
-	//initHighway(viewer);
+	constexpr int frame_per_sec = 30;
+    constexpr int sec_interval = 10;
+    constexpr double egoVelocity = 25;
 
-	int frame_per_sec = 30;
-	int sec_interval = 10;
-	int frame_count = 0;
-	int time_us = 0;
-
-	double egoVelocity = 25;
+    int frame_count = 0;
+    int time_us = 0;
 
 	while (frame_count < (frame_per_sec*sec_interval)) {
 		viewer->removeAllPointClouds();
 		viewer->removeAllShapes();
 
-		//stepHighway(egoVelocity,time_us, frame_per_sec, viewer);
-		highway.stepHighway(egoVelocity,time_us, frame_per_sec, viewer);
-		viewer->spinOnce(1000/frame_per_sec);
+		highway.stepHighway(egoVelocity, time_us, frame_per_sec, viewer);
+		viewer->spinOnce(1000 / frame_per_sec);
+
 		frame_count++;
 		time_us = 1000000*frame_count/frame_per_sec;
 	}
--- src/ukf.cpp
+++ src/ukf.cpp
@@ -8,83 +8,120 @@ using Eigen::VectorXd;
  * Initializes Unscented Kalman filter
  */
 UKF::UKF() {
-  // if this is false, laser measurements will be ignored (except during init)
-  use_laser_ = true;
+    // if this is false, laser measurements will be ignored (except during init)
+    use_laser_ = true;
 
-  // if this is false, radar measurements will be ignored (except during init)
-  use_radar_ = true;
+    // if this is false, radar measurements will be ignored (except during init)
+    use_radar_ = true;
 
-  // initial state vector
-  x_ = VectorXd(5);
+    // Process noise standard deviation longitudinal acceleration in m/s^2
+    std_a_ = 3.0;
 
-  // initial covariance matrix
-  P_ = MatrixXd(5, 5);
+    // Process noise standard deviation yaw acceleration in rad/s^2
+    std_yawdd_ = 0.5;
 
-  // Process noise standard deviation longitudinal acceleration in m/s^2
-  std_a_ = 30;
+    /**
+     * DO NOT MODIFY measurement noise values below.
+     * These are provided by the sensor manufacturer.
+     */
 
-  // Process noise standard deviation yaw acceleration in rad/s^2
-  std_yawdd_ = 30;
-  
-  /**
-   * DO NOT MODIFY measurement noise values below.
-   * These are provided by the sensor manufacturer.
-   */
+    // Laser measurement noise standard deviation position1 in m
+    std_laspx_ = 0.15;
 
-  // Laser measurement noise standard deviation position1 in m
-  std_laspx_ = 0.15;
+    // Laser measurement noise standard deviation position2 in m
+    std_laspy_ = 0.15;
 
-  // Laser measurement noise standard deviation position2 in m
-  std_laspy_ = 0.15;
+    // Radar measurement noise standard deviation radius in m
+    std_radr_ = 0.3;
 
-  // Radar measurement noise standard deviation radius in m
-  std_radr_ = 0.3;
+    // Radar measurement noise standard deviation angle in rad
+    std_radphi_ = 0.03;
 
-  // Radar measurement noise standard deviation angle in rad
-  std_radphi_ = 0.03;
+    // Radar measurement noise standard deviation radius change in m/s
+    std_radrd_ = 0.3;
 
-  // Radar measurement noise standard deviation radius change in m/s
-  std_radrd_ = 0.3;
-  
-  /**
-   * End DO NOT MODIFY section for measurement noise values 
-   */
-  
-  /**
-   * TODO: Complete the initialization. See ukf.h for other member properties.
-   * Hint: one or more values initialized above might be wildly off...
-   */
+    /**
+     * End DO NOT MODIFY section for measurement noise values
+     */
+
+    /**
+     * TODO: Complete the initialization. See ukf.h for other member properties.
+     * Hint: one or more values initialized above might be wildly off...
+     */
+    // Initially set to false, set to true in first call of ProcessMeasurement
+    is_initialized_ = false;
+
+    // State dimension
+    n_x_ = 5;
+
+    // Augmented state dimension
+    n_aug_ = 7;
+
+    // Sigma point spreading parameter
+    lambda_ = 3 - n_x_;
+
+    // initial state vector
+    x_ = VectorXd(n_x_);
+
+    // initial covariance matrix
+    P_ = MatrixXd(n_x_, n_x_);
+
+    // predicted sigma points matrix
+    Xsig_pred_ = MatrixXd(n_x_, 2 * n_aug_ + 1);;
+
+    // Weights of sigma points
+    weights_ = VectorXd(2 * n_aug_ + 1);
+
+//    //measurement noise covariance matrix
+//    R_radar_ = MatrixXd(3, 3);
+//    R_radar_ <<     std_radr_ * std_radr_, 0                         , 0,
+//            0                    , std_radphi_ * std_radphi_ , 0,
+//            0                    , 0                         , std_radrd_*std_radrd_;
+//
+//    R_laser_ = MatrixXd(2, 2);
+//    R_laser_ <<     std_laspx_ * std_laspx_, 0 ,
+//            0                      , std_laspy_ * std_laspy_ ;
+//
+//    NIS_radar_ = NIS_laser_ = 0;
+
+
 }
 
 void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
-  /**
-   * TODO: Complete this function! Make sure you switch between lidar and radar
-   * measurements.
-   */
+    /**
+     * TODO: Complete this function! Make sure you switch between lidar and radar
+     * measurements.
+     */
 }
 
 void UKF::Prediction(double delta_t) {
-  /**
-   * TODO: Complete this function! Estimate the object's location. 
-   * Modify the state vector, x_. Predict sigma points, the state, 
-   * and the state covariance matrix.
-   */
+    /**
+     * TODO: Complete this function! Estimate the object's location.
+     * Modify the state vector, x_. Predict sigma points, the state,
+     * and the state covariance matrix.
+     */
+
+//    MatrixXd Xsig_aug = MatrixXd(n_aug_, 2 * n_aug_ + 1);
+//    GenerateAugmentedSigmaPoints(&Xsig_aug);
+//    SigmaPointPrediction(Xsig_aug, delta_t);
+//    PredictMeanAndCovariance();
+
 }
 
 void UKF::UpdateLidar(MeasurementPackage meas_package) {
-  /**
-   * TODO: Complete this function! Use lidar data to update the belief 
-   * about the object's position. Modify the state vector, x_, and 
-   * covariance, P_.
-   * You can also calculate the lidar NIS, if desired.
-   */
+    /**
+     * TODO: Complete this function! Use lidar data to update the belief
+     * about the object's position. Modify the state vector, x_, and
+     * covariance, P_.
+     * You can also calculate the lidar NIS, if desired.
+     */
 }
 
 void UKF::UpdateRadar(MeasurementPackage meas_package) {
-  /**
-   * TODO: Complete this function! Use radar data to update the belief 
-   * about the object's position. Modify the state vector, x_, and 
-   * covariance, P_.
-   * You can also calculate the radar NIS, if desired.
-   */
+    /**
+     * TODO: Complete this function! Use radar data to update the belief
+     * about the object's position. Modify the state vector, x_, and
+     * covariance, P_.
+     * You can also calculate the radar NIS, if desired.
+     */
 }
